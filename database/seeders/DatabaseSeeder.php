5cf838a8a32cc0fe61dfcd5c7ec2a92bec89f8eb
Alberto Ammar <albertoammarw@gmail.com>
2021-04-09 05:29:47 -0300

    chore: add user seeder for local testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Users::factory([
+            'login' => 'john'
+        ])->create();
     }
 }
